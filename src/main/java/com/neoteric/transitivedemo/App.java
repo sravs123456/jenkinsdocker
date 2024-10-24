64638062823bc157af9b5ed4c3cde7edc00ec817
ADMIN <chennamsettisravya2002@gmail.com>
2024-10-24 12:58:55 +0530

    dockersquare

---
+++ src/main/java/com/neoteric/transitivedemo/App.java
@@ -0,0 +1,13 @@
+package com.neoteric.transitivedemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
